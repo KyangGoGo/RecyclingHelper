95a09bfd2b445050619c5eb4d83de6ed0d95b714
acisliver <7273322@naver.com>
2021-09-16 15:38:34 +0900

    Data.java 내용 입력

--- app/src/main/java/org/tensorflow/lite/examples/classification/data/Data.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/data/Data.java
@@ -6,9 +6,9 @@ public class Data {
 
     public static String[] items = {"paper", "can", "glass",
             "plastic", "plastic_bag", "battery", "styrofoam"};
-    public static String[] classification = {"c1", "c2", "c3", "c4", "c5", "c6", "c7"};
-    public static String[] discharge_day = {"d1", "d2", "d3", "d4", "d5", "d6", "d7"};
-    public static String[] explanation = {"e1", "e2", "e3", "e4", "e5", "e6", "e7"};
+    public static String[] classification = {"종이류", "캔류", "유리류", "플라스틱류", "비닐류", "폐전지", "폐스티로폼"};
+    public static String[] discharge_day = {"화, 목, 토", "매일", "화, 토", "월, 수, 금, 일", "목, 일", "매일", "매일"};
+    public static String[] explanation = {"물기에 젖지 않게 묶거나 박스류에 담아서 배출", "내용물을 비우고 배출", "내용물을 비우고 배출", "음식물 등 내용물을 깨끗이 비우고, 다른 재질 제거 후 압착하여 배출", "이물질을 씻어서 배출", "전용 수거함에 배출", "이물질을 제거하여 배출"};
     public static String[] additional_explanation = {"ae1", "ae2", "ae3", "ae4", "ae5", "ae6", "ae7"};
     public static Integer[] images = {R.drawable.paper, R.drawable.can, R.drawable.glass,
             R.drawable.plastic, R.drawable.plastic_bag, R.drawable.battery, R.drawable.styrofoam};
