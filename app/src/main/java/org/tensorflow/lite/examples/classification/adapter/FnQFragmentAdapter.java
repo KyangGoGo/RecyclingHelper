aac3b8bc9bca3a632fe36fc033be7d6e836a2c09
hunko <rhwlgns4386@naver.com>
2021-09-25 03:10:44 +0900

    말풍선 완성

--- app/src/main/java/org/tensorflow/lite/examples/classification/adapter/FnQFragmentAdapter.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/adapter/FnQFragmentAdapter.java
@@ -3,6 +3,7 @@ package org.tensorflow.lite.examples.classification.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,10 +16,12 @@ import java.util.ArrayList;
 
 public class FnQFragmentAdapter extends RecyclerView.Adapter<FnQFragmentAdapter.CustomViewHolder> {
     private ArrayList<FragementThirdData> arrayList;
-    private TextView visibleText;
+    private VisibleData visibleData;
+
 
     public FnQFragmentAdapter(ArrayList<FragementThirdData> arrayList) {
         this.arrayList = arrayList;
+        this.visibleData=new VisibleData();
     }
 
     @NonNull
@@ -39,15 +42,20 @@ public class FnQFragmentAdapter extends RecyclerView.Adapter<FnQFragmentAdapter.
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(visibleText!=null){
-                    if(visibleText!=holder.fnq_answer)visibleText.setVisibility(View.GONE);
-                    else if(visibleText.getVisibility()==View.VISIBLE){
-                        visibleText.setVisibility(View.GONE);
+                if(!visibleData.isEmpty()){
+                    if(visibleData.getVisibleText()!=holder.fnq_answer){
+                        visibleData.getVisibleText().setVisibility(View.GONE);
+                        visibleData.getVisibleImage().setVisibility(View.GONE);
+                    }
+                    else if(visibleData.getVisibleText().getVisibility()==View.VISIBLE){
+                        visibleData.getVisibleText().setVisibility(View.GONE);
+                        visibleData.getVisibleImage().setVisibility(View.GONE);
                         return;
                     }
                 }
                 holder.fnq_answer.setVisibility(View.VISIBLE);
-                visibleText=holder.fnq_answer;
+                holder.fnq_image.setVisibility(View.VISIBLE);
+                visibleData.setVisible(holder.fnq_answer,holder.fnq_image);
             }
         });
 
@@ -61,10 +69,38 @@ public class FnQFragmentAdapter extends RecyclerView.Adapter<FnQFragmentAdapter.
     public class CustomViewHolder extends RecyclerView.ViewHolder {
         protected TextView fnq_questions;
         protected TextView fnq_answer;
+        protected ImageView fnq_image;
         public CustomViewHolder(@NonNull View itemView) {
             super(itemView);
             this.fnq_questions=(TextView) itemView.findViewById(R.id.fnq_questions);
             this.fnq_answer=(TextView)itemView.findViewById(R.id.fnq_answer);
+            this.fnq_image=itemView.findViewById(R.id.fnq_answer_down);
         }
+    }
+    public class VisibleData{
+        private TextView visibleText;
+        private ImageView visibleImage;
+
+        public boolean isEmpty(){
+            if(visibleText==null || visibleImage==null){
+                return true;
+            }
+            return false;
+        }
+
+        public void setVisible(TextView visibleText,ImageView visibleImage) {
+            this.visibleText=visibleText;
+            this.visibleImage = visibleImage;
+        }
+
+        public TextView getVisibleText() {
+            return visibleText;
+        }
+
+        public ImageView getVisibleImage() {
+            return visibleImage;
+        }
+
+
     }
 }
